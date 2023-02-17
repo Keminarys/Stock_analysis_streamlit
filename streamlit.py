76eb0d08e72c0029ca294393ca8b0b3d6bd2334a
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-02-17 17:28:03 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,5 +11,5 @@ period_end = st.sidebar.date_input('Please enter ending date')
 ma_period = st.sidebar.text_input('Please enter a moving average period')
 interval = st.sidebar.selectbox('Please choose an interval', ['1d', '1w', '1m'])
 
-with st.expander('Parameters chosen'):
+with st.expander('Scope reminder'):
   st.write(f'Analysis is for {ticker} prices from {period_start} to {period_end} with an interval of {interval} and moving average is based on {ma_period} days.')
