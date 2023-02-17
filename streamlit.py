27a165a66f10e1f73f3943f93d100a1aaee93a61
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-02-17 17:24:19 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,11 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.title('Stock analyzer')
-
-with st.expander('Parameters chosen'):
-  st.write(f'Analysis is for {ticker} prices from {period_start} to {period_end} with an interval of {interval} and moving average is based on {ma_period} days.')
-  
+st.title('Stock analyzer')  
 
 st.sidebar.header('Parameters')
 ticker = st.sidebar.text_input('Please enter a ticker')
@@ -14,3 +10,6 @@ period_start = st.date_input('Please enter starting date')
 period_end = st.sidebar.text_input('Please enter ending date')
 ma_period = st.sidebar.text_input('Please enter a moving average period')
 interval = st.sidebar.selectbox('Please choose an interval', ['1d', '1w', '1m'])
+
+with st.expander('Parameters chosen'):
+  st.write(f'Analysis is for {ticker} prices from {period_start} to {period_end} with an interval of {interval} and moving average is based on {ma_period} days.')
