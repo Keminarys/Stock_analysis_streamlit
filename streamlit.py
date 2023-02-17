f30e1c435f17d28b1dadcbc5751938341547a724
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-02-17 17:26:16 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,8 +6,8 @@ st.title('Stock analyzer')
 
 st.sidebar.header('Parameters')
 ticker = st.sidebar.text_input('Please enter a ticker')
-period_start = st.date_input('Please enter starting date')
-period_end = st.sidebar.text_input('Please enter ending date')
+period_start = st.sidebar.date_input('Please enter starting date')
+period_end = st.sidebar.date_input('Please enter ending date')
 ma_period = st.sidebar.text_input('Please enter a moving average period')
 interval = st.sidebar.selectbox('Please choose an interval', ['1d', '1w', '1m'])
 
