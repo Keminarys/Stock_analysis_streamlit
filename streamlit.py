45fc21be9a2025186903d9dd96f31b275e1c3f6e
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-02-19 12:06:33 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -25,30 +25,30 @@ plots = st.sidebar.radio('Select a plot to show', pages)
 with st.expander('Scope reminder'):
   st.write(f'Analysis is for {ticker} prices from {period_start} to {period_end} with an interval of {interval} and moving average is based on {ma_period} days.')
 
-# period1 = int(time.mktime(period_start))
-# period2 = int(time.mktime(period_end))
+period1 = int(time.mktime(period_start))
+period2 = int(time.mktime(period_end))
 
-# url = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+url = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
 
-# df = pd.read_csv(url)
-# df['SMA'] = df['Close'].rolling(MA_period).mean()
-# df['EMA'] = df['Close'].ewm(span=MA_period).mean()
+df = pd.read_csv(url)
+df['SMA'] = df['Close'].rolling(MA_period).mean()
+df['EMA'] = df['Close'].ewm(span=MA_period).mean()
 
 
-# fig4 = go.Figure()
-# fig4.add_trace(go.Scatter(x=df["Date"], y=df["Close"],
-#                     mode='lines',
-#                     name='Close'))
-# fig4.add_trace(go.Scatter(x=df["Date"], y=df["SMA"],
-#                     mode='lines',
-#                     name='SMA'))
-# fig4.add_trace(go.Scatter(x=df["Date"], y=df["EMA"],
-#                     mode='lines', name='EMA'))
+fig4 = go.Figure()
+fig4.add_trace(go.Scatter(x=df["Date"], y=df["Close"],
+                    mode='lines',
+                    name='Close'))
+fig4.add_trace(go.Scatter(x=df["Date"], y=df["SMA"],
+                    mode='lines',
+                    name='SMA'))
+fig4.add_trace(go.Scatter(x=df["Date"], y=df["EMA"],
+                    mode='lines', name='EMA'))
 
-# fig4.update_layout(
-#     title=ticker+" Closing/SMA/EMA on "+str(MA_period)+" days",
-#     autosize=False,
-#     width=1500,
-#     height=700)
+fig4.update_layout(
+    title=ticker+" Closing/SMA/EMA on "+str(MA_period)+" days",
+    autosize=False,
+    width=1500,
+    height=700)
 
-# if plots == 'Prices' : st.plotly_chart(fig4)
+if plots == 'Prices' : st.plotly_chart(fig4)
