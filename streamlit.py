34df362f7edc8b96b9221f00f3cd48d56caaf0f8
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-02-22 11:20:14 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,15 +12,16 @@ st.set_page_config(layout="wide")
 
 st.title('Stock analyzer')  
 
-pages = ['Prices','Candlesticks','Volume','Forecast', 'General Info']
+pages = ['Prices','Candlesticks','Volume','General Info']
 
-st.sidebar.header('Parameters')
-ticker = st.sidebar.text_input('Please enter a ticker')
-period_start = st.sidebar.date_input('Please enter starting date')
-period_end = st.sidebar.date_input('Please enter ending date')
-ma_period = st.sidebar.text_input('Please enter a moving average period')
-interval = st.sidebar.selectbox('Please choose an interval', ['1d', '1wk', '1mo'])
-plots = st.sidebar.radio('Select a plot to show', pages)
+with st.sidebar.expander("General Input"):
+  st.write("This section is used for stock analysis")
+  ticker = st.sidebar.text_input('Please enter a ticker')
+  period_start = st.sidebar.date_input('Please enter starting date')
+  period_end = st.sidebar.date_input('Please enter ending date')
+  ma_period = st.sidebar.text_input('Please enter a moving average period')
+  interval = st.sidebar.selectbox('Please choose an interval', ['1d', '1wk', '1mo'])
+  plots = st.sidebar.radio('Select a plot to show', pages)
 
 with st.expander('Scope reminder'):
   st.write(f'Analysis is for {ticker} prices from {period_start} to {period_end} with an interval of {interval} and moving average is based on {ma_period} days.')
