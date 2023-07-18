c1d4faf9328e8b25411b759dfd646f1364ec7e91
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-18 18:20:45 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -384,7 +384,7 @@ if plots_f == 'Yes' :
         
     with st.container() :
         if len(portfolio_) > 0 :
-        st.write("Performance of the chosen tickers")
+            st.write("Performance of the chosen tickers")
             for i in portfolio_ :
                 df_temp = pd.read_csv(f'https://query1.finance.yahoo.com/v7/finance/download/{i}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true')
                 df_temp['Ticker'] = i
